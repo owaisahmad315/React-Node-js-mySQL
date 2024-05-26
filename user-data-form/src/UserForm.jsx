2cf9d48f64966c0e8059c023473b5c1b6aa48f33
Owais Ahmad <owaisahmad014@gmail.com>
2024-05-26 15:25:48 +0500

    done

--- user-data-form/src/UserForm.jsx
+++ user-data-form/src/UserForm.jsx
@@ -1,36 +1,77 @@
 import React, {useState} from "react";
 import axios from 'axios';
+import './UserForm.css'
 
 const UserForm = () => {
 
+    const [formData, setFormData ] = useState({
+        name: '',
+        father_name: '',
+        age: '',
+        education: '',
+        phone_number: '',
+        email: ''
+    })
+
+
+    const handleChange = (e) => {
+        const {name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:3001/api/users', formData)
+        .then(Response => {
+            alert('User Data Added to Database Successfully!');
+            setFormData({
+                name: '',
+                father_name: '',
+                age: '',
+                education: '',
+                phone_number: '',
+                email: '' 
+            });
+        })
+        .catch(error => {
+            console.log('There was an ', error)
+        })
+    }
 
     return (
         <form onSubmit={handleSubmit}>
             <div>
                 <label>Name:</label>
-                <input type="text" name='name' value={FormData.name} onChange={handleChange} required />
+                <input type="text" name='name' value={formData.name} onChange={handleChange} required />
             </div>
             <div>
                 <label>Father Name:</label>
-                <input type="text" name='fatherName' value={FormData.fatherName} onChange={handleChange} required />
+                <input type="text" name='fatherName' value={formData.father_name} onChange={handleChange} required />
             </div>
             <div>
                 <label>Age:</label>
-                <input type="number" name='age' value={FormData.age} onChange={handleChange} required />
+                <input type="number" name='age' value={formData.age} onChange={handleChange} required />
             </div>
             <div>
                 <label>Education:</label>
-                <input type="text" name='education' value={FormData.education} onChange={handleChange} required />
+                <input type="text" name='education' value={formData.education} onChange={handleChange} required />
             </div>
             <div>
                 <label>Phone Number:</label>
-                <input type="number" name='phoneNumber' value={FormData.phoneNumber} onChange={handleChange} required />
+                <input type="number" name='phoneNumber' value={formData.phone_number} onChange={handleChange} required />
             </div>
             <div>
                 <label>Email:</label>
-                <input type="email" name='email' value={FormData.email} onChange={handleChange} required />
+                <input type="email" name='email' value={formData.email} onChange={handleChange} required />
             </div>
             <button type="submit" >Submit</button>
         </form>
     )
 }
+
+
+export default UserForm;
